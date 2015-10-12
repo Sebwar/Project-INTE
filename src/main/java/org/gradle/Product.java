d6f8b04a5771ee1da92fa4dd6cc060b897baceda
Sebastian Wärnström <sebwar01@hotmail.com>
2015-10-12 20:22:54 +0200

    Produktklassen gjord.

--- src/main/java/org/gradle/Product.java
+++ src/main/java/org/gradle/Product.java
@@ -1,5 +1,23 @@
 package org.gradle;
 
 public class Product {
-
+	private int id;
+	private String name;
+	private int price;
+	private boolean weightPriced;
+	
+	public Product(int id, String name, int price, boolean weightPriced) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.weightPriced = weightPriced;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	public boolean isWeightPriced() {
+		return weightPriced;
+	}
 }
