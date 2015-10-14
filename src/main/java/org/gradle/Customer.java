df0b848c5d397dd9aeed38a1abb72f0a036ed8d5
torbhkan <torbhkan@gmail.com>
2015-10-14 20:26:04 +0200

    Uppdaterat Customer samt lagt till Documentation v1

--- src/main/java/org/gradle/Customer.java
+++ src/main/java/org/gradle/Customer.java
@@ -2,33 +2,34 @@ package org.gradle;
 
 public class Customer {
 	
+	private String orgNr;
 	private String firstName;
 	private String lastName;
 	private String street;
 	private String address;
+	private String phone;
 	
 	//Construct
-	public Customer (String firstName, String lastName, String address, String street)
+	public Customer (String orgNr, String firstName, String lastName, String address, String street, String phone)
     {
+		this.firstName = orgNr;
         this.firstName = firstName;
         this.lastName = lastName;
         this.street = street;
         this.address = address;
+        this.address = phone;
     }
 	
 	public String getFullName() {
 	    return firstName + " " + lastName;
 	}
 	
-	public String getAdress() {
-	    return street + " " + address;
-	}
-	
+
 	@Override		
 	public String toString()
     {
         //Create a String that represents this object
-        return firstName + " " + lastName + "\n" + street + "\n" + address;
+        return orgNr + "\n" + firstName + " " + lastName + "\n" + street + "\n" + address + "\n" + phone;
     }
 	
 	
