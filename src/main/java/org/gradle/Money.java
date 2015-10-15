44c7bb522e15babb4740d845b629b8a03abfd480
Sebastian Wärnström <sebwar01@hotmail.com>
2015-10-15 20:06:30 +0200

    Mer gjort på Money-klassen.

--- src/main/java/org/gradle/Money.java
+++ src/main/java/org/gradle/Money.java
@@ -1,12 +1,37 @@
 package org.gradle;
 
-public class Money {
+public class Money implements Comparable<Money> {
 	private static final int MAJOR_UNIT = 100;
 	
 	private Currency currency;
-	private long amount;
+	private long amount;  //Stored in minor units, e.g. cents or öre.
 	
 	public Money(Currency currency, long amount) {
-		
+		if(currency == null)
+			throw new IllegalArgumentException("Currency cannot be null.");
+		if(amount < 0)
+			throw new IllegalArgumentException("Amount cannot be negative.");
+		this.currency = currency;
+		this.amount = amount;
+	}
+	
+	public Money(Currency currency, long amountInMajorUnit, long amountInMinorUnit) {
+		this(currency, amountInMajorUnit * MAJOR_UNIT + amountInMinorUnit);
+	}
+	
+	public long getTotalAmountInMinorUnit() {
+		return amount;
+	}
+	
+	public int compareTo(Money other) {
+		if(other == null)
+			throw new IllegalArgumentException("Money cannot be null.");
+		if(other.currency != currency)
+			throw new IllegalArgumentException("The money has to be of the same currency.");
+		return (int)(amount - other.amount);
+	}
+	
+	public String toString() {
+		return null;
 	}
 }
--- src/test/java/org/gradle/MoneyTest.java
+++ src/test/java/org/gradle/MoneyTest.java
@@ -1,5 +1,6 @@
 package org.gradle;
 
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MoneyTest {
@@ -11,12 +12,53 @@ public class MoneyTest {
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
-	public void testConstructorWithNullCurrency() {
+	public void testConstructorNullCurrency() {
 		money = new Money(null, 10);
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
-	public void testConstructorWithNegativeAmount() {
+	public void testConstructorNegativeAmount() {
 		money = new Money(Currency.EUR, -1);
+	}
+	
+	@Test
+	public void testConvenienceConstructor() {
+		money = new Money(Currency.SEK, 47, 30);
+		assertEquals(4730, money.getTotalAmountInMinorUnit());
+	}
+	
+	@Test
+	public void testCompareToLessThan() {
+		money = new Money(Currency.EUR, 111);
+		Money other = new Money(Currency.EUR, 110);
+		assertTrue(money.compareTo(other) > 0);
+	}
+	
+	@Test
+	public void testCompareToGreaterThan() {
+		money = new Money(Currency.EUR, 56);
+		Money other = new Money(Currency.EUR, 57);
+		assertTrue(money.compareTo(other) < 0);
+	}
+	
+	@Test
+	public void testCompareToEqualTo() {
+		money = new Money(Currency.EUR, 71);
+		Money other = new Money(Currency.EUR, 71);
+		assertTrue(money.compareTo(other) == 0);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testCompareToNull() {
+		money = new Money(Currency.EUR, 111);
+		Money other = null;
+		money.compareTo(other);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testCompareDifferentCurrencies() {
+		money = new Money(Currency.EUR, 110);
+		Money other = new Money(Currency.USD, 110);
+		assertTrue(money.compareTo(other) == 0);
 	}
 }
