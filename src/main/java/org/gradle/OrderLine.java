5d931791ca44461fc72579b0234c6b00190f6063
Niklas Anner <niklas@anner.se>
2015-10-18 14:54:54 +0200

    Preliminär toString()-metod

--- src/main/java/org/gradle/OrderLine.java
+++ src/main/java/org/gradle/OrderLine.java
@@ -5,11 +5,15 @@ public class OrderLine {
 	private Product product;
 	private int quantity;
 	private int price;
+	private int totalPrice;
+	private int productID;
 	
 	public OrderLine (Product product, int quantity) {
 		this.product = product;		
 		this.quantity = quantity;
 		price = product.getPrice();
+		totalPrice = price * quantity;
+		productID = product.getID();
 			
 	}
 	
@@ -17,7 +21,17 @@ public class OrderLine {
 		return price * quantity;
 	}
 	
+	//ProductID används tills getName()-metod har implementerats i Product.java
+	
 	public String toString() {
-		return null;
+		if(product.isWeightPriced() == true){
+			return "ProduktID "+productID+"  "+quantity+"g*"+price+"öre/g  "+totalPrice+"öre";
+				
+		}
+		else {
+			return "ProduktID "+productID+"  "+quantity+"st*"+price+"öre  "+totalPrice+"öre";
+		}
+			
+		
 	}
 }
--- src/test/java/org/gradle/OrderLineTest.java
+++ src/test/java/org/gradle/OrderLineTest.java
@@ -4,22 +4,43 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class OrderLineTest {
+	
+	Product tk = new Product(240, "Tandkräm", 25, false);
+	Product tb = new Product(67, "Tandborste", 35, false);
+	Product k = new Product(58, "Kaffe", 14, true);
+	Product h = new Product(463, "Högrev", 16, true);
 
+	OrderLine o1s = new OrderLine(tk, 2);
+	OrderLine o2s = new OrderLine(tb, 4);
+	OrderLine o1k = new OrderLine(k, 50);
+	OrderLine o2k = new OrderLine(h, 150);
+	
 	@Test
-	public void testConstructorStyck() {
-		Product p = new Product(240, "Tandkräm", 25, false);
-		OrderLine ol = new OrderLine(p, 2);
+	public void testConstructorStyck() {//pris i öre/st
 		
-		assertEquals(50, ol.getTotalPrice());
-			
+		assertEquals(50, o1s.getTotalPrice());
+		assertEquals(140, o2s.getTotalPrice());
+	}
+	@Test
+	public void testConstructorKilo(){//pris i öre/g
+		
+		assertEquals(700, o1k.getTotalPrice());
+		assertEquals(2400, o2k.getTotalPrice());
+	}
+	@Test
+	public void testTostringStyck(){
+		
+		assertEquals("ProduktID 240  2st*25öre  50öre", o1s.toString());
+		assertEquals("ProduktID 67  4st*35öre  140öre", o2s.toString());
 	}
 	
-	public void testConstructorKilo(){
-		Product p = new Product(58, "Kaffe", 14, true);
-		OrderLine ol = new OrderLine(p, 50);
-	
-		assertEquals(700, ol.getTotalPrice());
+	@Test
+	public void testTostringKilo(){
+		
+		assertEquals("ProduktID 58  50g*14öre/g  700öre", o1k.toString());
+		assertEquals("ProduktID 463  150g*16öre/g  2400öre", o2k.toString());
 		
 	}
+	
 	
 }
