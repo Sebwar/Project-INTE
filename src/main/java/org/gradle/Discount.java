053d8a803a32f8690c5d065fe3558bf3b7027efb
Pontus Valldor <pontus.valldor@me.com>
2015-10-21 19:02:11 +0200

    Lade till to-String metod i discount klasserna och nu kan man lägga till rabatter för kategorier

--- src/main/java/org/gradle/Discount.java
+++ src/main/java/org/gradle/Discount.java
@@ -7,34 +7,39 @@ public abstract class Discount {
 	
 	protected Date startTime;
 	protected Date endTime;
-	protected ArrayList<Integer> productIDs;
+	protected ArrayList<Integer> itemIDs;
+	protected boolean categoryDiscount;
 	
 	/* When creating a new Discount you need to know
 	 * when it should start, when it should end and for what products it is for
 	 * 
-	 * productID cannot be negative
+	 * itemID cannot be negative
 	 * endTime cannot be before startTime
 	 */
-	public Discount(Date startTime, Date endTime, int productID) {
-		if (productID < 0)
-			throw new IllegalArgumentException("Product ID cannot be negative.");
+	public Discount(Date startTime, Date endTime, int itemID, boolean categoryDiscount) {
+		if (itemID < 0)
+			throw new IllegalArgumentException("Product/Category ID cannot be negative.");
 		
 		if (endTime.getTime() < startTime.getTime())
 			throw new IllegalArgumentException("End time cannot be earlier than start time.");
 		
-		productIDs = new ArrayList<Integer>();
-		productIDs.add(productID);
+		itemIDs = new ArrayList<Integer>();
+		itemIDs.add(itemID);
+		this.categoryDiscount = categoryDiscount;
 	}
 	
-	public void addProduct(int productID) {
-		if (productID < 0)
+	public boolean isDiscounted(int itemID, boolean category) {
+		if (itemIDs.contains(itemID) && categoryDiscount == category)
+			return true;
+		else
+			return false;
+	}
+	
+	public void addItem(int itemID) {
+		if (itemID < 0)
 			throw new IllegalArgumentException();
 		
-		productIDs.add(productID);
-	}
-	
-	public boolean isDiscounted(Product product) {
-		return productIDs.contains(product.getID());
+		itemIDs.add(itemID);
 	}
 	
 	public abstract int apply(OrderLine orderLine);
